07ca4e57d9531be0a77da0c0318411df8fe0bf2d
greekavi99 <greeta1999kavitha@gmail.com>
2026-05-09 16:41:34 +0530

    added family puzzle game and pacman

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Birthday from "./Birthday";
 import MonthGame from "./MonthGame";
+import PuzzleGame from "./PuzzleGame";
+import PacManGame from "./PacManGame";
+import ScoreBoard from "./ScoreBoard";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Birthday />} />
         <Route path="/game" element={<MonthGame />} />
+        <Route path="/puzzle" element={<PuzzleGame />} />
+        <Route path="/pacman" element={<PacManGame />} />
+        <Route path="/scores" element={<ScoreBoard />} />
       </Routes>
     </BrowserRouter>
   );
